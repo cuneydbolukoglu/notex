37468070ddfd810a7a90e2d72597cb88b91d875d
Cüneyd BÖLÜKOĞLU <cuneydbolukogluu@gmail.com>
2025-03-18 10:41:44 +0300

    [feat] Login page has been edited

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,58 +1,107 @@
-import { Container, Box, Typography, TextField, FormControlLabel, Checkbox, Button, Link, Paper } from '@mui/material';
+import { Container, Box, Typography, TextField, FormControlLabel, Checkbox, Button, Link, Paper, Divider } from '@mui/material';
+import { useEffect, useState } from 'react';
+import { useRouter } from "next/navigation";
+import utils from '@/utils';
+import { Formik, Form, Field } from 'formik';
+import * as Yup from 'yup';
+import axiosInstance from '@/services/axiosInstance';
 
-export default function Login() {    
+export default function Login() {
+    const [token, setToken] = useState(null);
+    const FIREBASE_API_KEY = process.env.NEXT_PUBLIC_FIREBASE_API_KEY;
+    const router = useRouter();
+
+    useEffect(() => {
+        if (token !== null) {
+            utils.cookieManager.set("token", token);
+        }
+    }, [token]);
+
+    const signIn = async (values, { setSubmitting }) => {
+        const URL = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${FIREBASE_API_KEY}`;
+        try {
+            const response = await axiosInstance.post(URL, {
+                email: values.email,
+                password: values.password,
+                returnSecureToken: true
+            }, {
+                headers: { "Content-Type": "application/json" }
+            });
+            const idToken = response.data.idToken;
+            setToken(idToken);
+            utils.cookieManager.set("token", idToken);
+            router.push("/");
+        } catch (error) {
+            console.error("Login error:", error);
+        }
+        setSubmitting(false);
+    };
+
+    const validationSchema = Yup.object({
+        email: Yup.string().email("Geçerli bir email girin").required("Email zorunludur"),
+        password: Yup.string().min(6, "Şifre en az 6 karakter olmalıdır").required("Şifre zorunludur")
+    });
+
     return (
-        <Container component="main" maxWidth="xs">
+        <Container className='auth' component="main" maxWidth="xs">
             <Paper elevation={6} sx={{ padding: 4, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-                <Typography component="h1" variant="h5">
-                    Sign in
-                </Typography>
-                <Box component="form" noValidate sx={{ mt: 1 }}>
-                    <TextField
-                        margin="normal"
-                        required
-                        fullWidth
-                        id="email"
-                        label="Email Address"
-                        name="email"
-                        autoComplete="email"
-                        autoFocus
-                    />
-                    <TextField
-                        margin="normal"
-                        required
-                        fullWidth
-                        name="password"
-                        label="Password"
-                        type="password"
-                        id="password"
-                        autoComplete="current-password"
-                    />
-                    <FormControlLabel
-                        control={<Checkbox value="remember" color="primary" />}
-                        label="Remember me"
-                    />
-                    <Button
-                        type="submit"
-                        fullWidth
-                        variant="contained"
-                        sx={{ mt: 3, mb: 2 }}
-                    >
-                        Sign In
-                    </Button>
-
-                    <Link href="/forgot-password" variant="body2">
-                        Forgot password?
-                    </Link>
-                    <Button
-                        type="submit"
-                        fullWidth
-                        variant="outlined"
-                        sx={{ mt: 3, mb: 2 }}
-                    >
-                        Sign in with Google
-                    </Button>
-                </Box>
+                <Typography component="h1" variant="h5">Sign in</Typography>
+                <Formik
+                    initialValues={{ email: '', password: '' }}
+                    validationSchema={validationSchema}
+                    onSubmit={signIn}
+                >
+                    {({ isSubmitting, errors, touched }) => (
+                        <Form>
+                            <Field as={TextField}
+                                margin="normal"
+                                required
+                                fullWidth
+                                id="email"
+                                label="Email Address"
+                                name="email"
+                                autoComplete="email"
+                                autoFocus
+                                error={touched.email && !!errors.email}
+                                helperText={touched.email && errors.email}
+                            />
+                            <Field as={TextField}
+                                margin="normal"
+                                required
+                                fullWidth
+                                name="password"
+                                label="Password"
+                                type="password"
+                                id="password"
+                                autoComplete="current-password"
+                                error={touched.password && !!errors.password}
+                                helperText={touched.password && errors.password}
+                            />
+                            <FormControlLabel
+                                control={<Checkbox value="remember" color="primary" />}
+                                label="Remember me"
+                            />
+                            <Button
+                                type="submit"
+                                fullWidth
+                                variant="contained"
+                                sx={{ mt: 3, mb: 2 }}
+                                disabled={isSubmitting}
+                            >
+                                Sign In
+                            </Button>
+                            <Link href="/forgot-password" variant="body2">Forgot password?</Link>
+                            <Divider>or</Divider>
+                            <Button
+                                fullWidth
+                                sx={{ mt: 3, mb: 2 }}
+                                disabled={isSubmitting}
+                            >
+                                Sign in with Google
+                            </Button>
+                        </Form>
+                    )}
+                </Formik>
             </Paper>
         </Container>
     );
