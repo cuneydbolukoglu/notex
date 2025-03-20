da0707a656a9ad3012548c101ae72bb1f052cbc9
Cüneyd BÖLÜKOĞLU <cuneydbolukogluu@gmail.com>
2025-03-20 14:07:59 +0300

    [feat] Layout page & ThemeProvider edited

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,41 @@
+import { useEffect, useState } from "react";
 import Layout from "@/components/layout";
 import "@/styles/globals.css";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { useRouter } from "next/router";
+import { CssBaseline } from "@mui/material";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
+  const [themeMode, setThemeMode] = useState("dark");
+  const router = useRouter();
+  const noLayoutPages = ["/login"];
+
+  const isNoLayoutPage = noLayoutPages.includes(router.pathname);
+
+  useEffect(() => {
+    const savedTheme = localStorage.getItem("theme");
+    if (savedTheme) {
+      setThemeMode(savedTheme);
+    }
+  }, []);
+
+  const customizeTheme = createTheme({
+    palette: {
+      mode: themeMode === "light" ? "light" : "dark",
+    },
+  });
+
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  )
+    <ThemeProvider theme={customizeTheme}>
+      <Head>
+        <title>NoteX</title>
+        <meta name="description" content="notex your fast and easy note application" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <CssBaseline />
+      {isNoLayoutPage ? <Component {...pageProps} /> : <Layout><Component {...pageProps} /></Layout>}
+    </ThemeProvider>
+  );
 }
