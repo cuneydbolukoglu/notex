9a4e780d7a983807fbc0d6aec12415d8cc14c60a
Cüneyd BÖLÜKOĞLU <cuneydbolukogluu@gmail.com>
2025-03-28 12:10:33 +0300

    [feat] All notes components edited

--- src/components/allnotes/index.js
+++ src/components/allnotes/index.js
@@ -1,46 +1,84 @@
-import Button from '@mui/material/Button';
+import { useEffect, useState } from "react";
+import { Button, Card, CardContent, Chip, Grid, Typography, Divider, Box } from "@mui/material";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlus } from "@fortawesome/free-solid-svg-icons";
-import { useEffect } from 'react';
+import { faClock, faPlus, faTag } from "@fortawesome/free-solid-svg-icons";
 import axiosInstance from "@/services/axiosInstance";
-import utils from '@/utils';
+import SelectedNote from "../selectednote";
 
 export default function AllNotes() {
+    const [allNotes, setAllNotes] = useState([]);
+    const [selectedNote, setSelectedNote] = useState(null);
+
     useEffect(() => {
-        // addNote();
         getAllNotes();
     }, []);
-
-    const addNote = async () => {
-        try {
-            const response = await axiosInstance.post("/all_notes.json", {
-                title: "React Performance",
-                content: "Lorem ipsum dolar",
-                tags: ["react"],
-                created_date: utils.getCurrentDate(),
-                updated_date: null,
-            }, {
-                headers: {
-                    "Content-Type": "application/json"
-                }
-            });
-
-            console.log(response.data);
-        } catch (error) {
-            console.error("Not ekleme hatası:", error);
-        }
-    };
 
     const getAllNotes = async () => {
         try {
             const response = await axiosInstance.get("/all_notes.json");
-            console.log(response.data);
+            const notesArray = Object.values(response.data);
+            setAllNotes(notesArray);
+            if (notesArray.length > 0) {
+                setSelectedNote(notesArray[0]); // İlk notu varsayılan olarak seç
+            }
         } catch (error) {
             console.error("Veri çekme hatası:", error);
         }
     };
 
     return (
-        <div>All notes</div>
-    )
+        <Grid container spacing={2}>
+            <Grid item xs={12} sm={3}>
+                <Button fullWidth variant="contained">
+                    <FontAwesomeIcon style={{ marginRight: 5 }} icon={faPlus} /> Create new note
+                </Button>
+
+                <Box mt={2}>
+                    {allNotes.map((item, index) => (
+                        <Card
+                            key={index}
+                            sx={{
+                                marginBottom: 2,
+                                cursor: "pointer",
+
+                            }}
+                            onClick={() => setSelectedNote(item)}
+                        >
+                            <CardContent>
+                                <Typography variant="h6">{item.title}</Typography>
+                                <Box mt={1}>
+                                    {item.tags.map((tag, tagIndex) => (
+                                        <Chip
+                                            key={tagIndex}
+                                            label={tag}
+                                            sx={{ marginRight: 1, borderRadius: 1, height: 25 }}
+                                        />
+                                    ))}
+                                </Box>
+                                <Typography
+                                    variant="body2"
+                                    color="text.secondary"
+                                    sx={{ marginTop: 1 }}
+                                >
+                                    {item.created_date}
+                                </Typography>
+                            </CardContent>
+                        </Card>
+                    ))}
+                </Box>
+            </Grid>
+
+            <Grid item xs={12} sm={9}>
+                {selectedNote ? (
+                    <SelectedNote
+                        selectedNote={selectedNote}
+                    />
+                ) : (
+                    <Typography variant="body1" color="text.secondary">
+                        Henüz bir not seçilmedi.
+                    </Typography>
+                )}
+            </Grid>
+        </Grid>
+    );
 }
