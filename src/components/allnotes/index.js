b7ad24174be1d766f99f9f26432af3d3776700f3
Cüneyd BÖLÜKOĞLU <cuneydbolukogluu@gmail.com>
2025-04-24 09:36:34 +0300

    [feat] Allnotes component updated

--- src/components/allnotes/index.js
+++ src/components/allnotes/index.js
@@ -83,7 +83,7 @@ export default function AllNotes() {
                     )
                 }
                 {
-                    createNote &&
+                    selectedNote === null && createNote &&
                     (
                         <CreateNote />
                     )
