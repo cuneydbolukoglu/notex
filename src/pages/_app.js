32e3ef70da1652fb4f84820ace246b0159489104
Cüneyd BÖLÜKOĞLU <cuneydbolukogluu@gmail.com>
2025-05-30 09:28:51 +0300

    [feat] customizeTheme added

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,80 +1,24 @@
 import { useEffect, useState } from "react";
 import Layout from "@/components/layout";
 import "@/styles/globals.css";
-import { ThemeProvider, createTheme } from "@mui/material/styles";
-import { useRouter } from "next/router";
+import { ThemeProvider } from "@mui/material/styles";
 import { CssBaseline } from "@mui/material";
 import Head from "next/head";
+import { usePathname } from "next/navigation";
+import ErrorGlobalProvider from "@/components/globalError";
+import customizeTheme from "@/theme";
 
 export default function App({ Component, pageProps }) {
   const [themeMode, setThemeMode] = useState("dark");
-  const noLayoutPages = ["/login", "/sign-up"];
-  const router = useRouter();
-  const [isClient, setIsClient] = useState(false);
-
-  // const isNoLayoutPage = noLayoutPages.includes(router.pathname);
+  const pathname = usePathname();
+  const isAuthPage = pathname.startsWith("/auth");
 
   useEffect(() => {
-    setIsClient(true);
-
     const savedTheme = localStorage.getItem("theme");
     if (savedTheme) {
       setThemeMode(savedTheme);
     }
-    console.log("test");
   }, []);
-
-  useEffect(() => {
-    console.log("Router:", router);
-    console.log("Router isReady:", router.isReady);
-    console.log("Window Pathname:", typeof window !== "undefined" ? window.location.pathname : "undefined");
-  }, [router]);
-
-    // ❗ Bu kontrolü buraya alıyoruz ve güvenli hale getiriyoruz
-    const isNoLayoutPage =
-    typeof window !== "undefined" &&
-    ["/login", "/sign-up"].includes(window.location.pathname);
-
-
-  const customizeTheme = createTheme({
-    palette: {
-      mode: themeMode === "light" ? "light" : "dark",
-    },
-    components: {
-      MuiButton: {
-        styleOverrides: {
-          root: {
-            textTransform: 'none',
-          },
-        },
-      },
-      MuiTextField: {
-        styleOverrides: {
-          root: {
-            '& .MuiOutlinedInput-root': {
-              '& fieldset': {
-                boxShadow: 'none',
-                '-webkit-box-shadow': 'none',
-              },
-              '&:hover fieldset': {
-                boxShadow: 'none',
-              },
-              '&.Mui-focused fieldset': {
-                boxShadow: 'none',
-              },
-              '& .MuiInputBase-input.MuiOutlinedInput-input:-webkit-autofill': {
-                backgroundColor: 'transparent',
-                color: 'inherit',
-                boxShadow: 'none',
-              },
-            },
-          },
-        },
-      }
-    },
-  });
-
-  if (!isClient) return null;
 
   return (
     <ThemeProvider theme={customizeTheme}>
@@ -85,7 +29,9 @@ export default function App({ Component, pageProps }) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <CssBaseline />
-      {isNoLayoutPage ? <Component {...pageProps} /> : <Layout pageTitle={Component.pageTitle}><Component {...pageProps} /></Layout>}
+      <ErrorGlobalProvider>
+        {isAuthPage ? <Component {...pageProps} /> : <Layout pageTitle={Component.pageTitle}><Component {...pageProps} /></Layout>}
+      </ErrorGlobalProvider>
     </ThemeProvider>
   );
 }
---
+++ src/theme/index.js
@@ -0,0 +1,74 @@
+import { createTheme } from '@mui/material/styles';
+
+const customizeTheme = createTheme({
+    palette: {
+        mode: "dark",
+    },
+    components: {
+        MuiButton: {
+            styleOverrides: {
+                root: {
+                    textTransform: 'none',
+                    borderRadius: 8,
+                },
+            },
+        },
+        MuiTextField: {
+            styleOverrides: {
+                root: {
+                    '& .MuiOutlinedInput-root': {
+                        '& fieldset': {
+                            boxShadow: 'none',
+                            '-webkit-box-shadow': 'none',
+                        },
+                        '&:hover fieldset': {
+                            boxShadow: 'none',
+                        },
+                        '&.Mui-focused fieldset': {
+                            boxShadow: 'none',
+                        },
+                        '& .MuiInputBase-input.MuiOutlinedInput-input:-webkit-autofill': {
+                            backgroundColor: 'transparent',
+                            color: 'inherit',
+                            boxShadow: 'none',
+                        },
+                    },
+                },
+            },
+        },
+        MuiInput: {
+            styleOverrides: {
+                root: {
+                    '&:before': {
+                        borderBottom: '0px !important',
+                    },
+                    '&:after': {
+                        borderBottom: '0px !important',
+                    },
+                    '&:hover:not(.Mui-disabled):before': {
+                        borderBottom: '0px !important',
+                    },
+                },
+            },
+        },
+        MuiPaper: {
+            styleOverrides: {
+                root: {
+                    borderRadius: 8,
+                    padding: '5px 0'
+                }
+            }
+        },
+        MuiOutlinedInput: {
+            styleOverrides: {
+                root: {
+                    height: 40,
+                    borderRadius: 8,
+                    background: '#3b3b3b'
+                }
+            }
+        }
+    },
+});
+
+export default customizeTheme;
