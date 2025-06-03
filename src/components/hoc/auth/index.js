e2cfd40e5312fdad6c6a6a5fa17ed87eb085b23a
Cüneyd BÖLÜKOĞLU <cuneydbolukogluu@gmail.com>
2025-06-03 10:45:58 +0300

    [feat] Auth component updated

--- src/components/hoc/auth/index.js
+++ src/components/hoc/auth/index.js
@@ -1,25 +1,31 @@
 import { useEffect, useState } from "react";
-import { useRouter } from "next/navigation";
+import { useRouter, usePathname } from "next/navigation";
 import utils from "@/utils";
 
 const withAuth = (WrappedComponent) => {
   return function AuthComponent(props) {
     const router = useRouter();
+    const pathname = usePathname();
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
       if (typeof window !== "undefined") {
         const token = utils.cookieManager.get("token");
+        const isAuthPage = pathname.startsWith("/auth");
 
-        if (!token) {
-          router.push("/login"); // Token yoksa login'e yönlendir
-        } else if (router.pathname === "/login") {
+        // Token yoksa ve auth sayfasında değilse login'e yönlendir
+        if (!token && !isAuthPage) {
+          router.push("/auth/login");
+        }
+
+        // Token varsa ama auth sayfasındaysa anasayfaya yönlendir
+        if (token && isAuthPage) {
           router.push("/");
         }
-        
+
         setLoading(false);
       }
-    }, []);
+    }, [pathname]);
 
     if (loading) return null;
 
